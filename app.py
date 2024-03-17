2eefd375eb5ce1f124b76352e0e6e77ae797776f
Dre18 <andrecunningham1@gmail.com>
2024-03-16 22:24:48 -0500

    v1.01

--- app.py
+++ app.py
@@ -17,16 +17,16 @@ def db_connection():
 
 
 
-@app.route('/hello_world', methods=['GET'])
-def hello_world():
-    return "hello world"
+# @app.route('/hello_world', methods=['GET'])
+# def hello_world():
+#     return "hello world"
 
 
 
 
 
 
-@app.route('/get_customers', methods=['GET'])
+@app.route('/customers', methods=['GET'])
 def get_customers():
     try:
         with db_connection() as db:
@@ -50,7 +50,7 @@ def get_customers():
     except Exception as e:
         return make_response({'error': str(e)}, 400)
 
-@app.route('/get_customer/<customer_id>', methods=['GET'])
+@app.route('/customer/<customer_id>', methods=['GET'])
 def get_customer(customer_id):
     try:
         with db_connection() as db:
@@ -76,34 +76,34 @@ def get_customer(customer_id):
     except:
         return make_response({'error': 'An error has occured'}, 400)
 
-# @app.route('/add_customer', methods=['POST'])
-# def add_customer():
-#     try:
-#         db = mysql.connector.connect(
-#             host = "localhost",
-#             user = "root",
-#             password = "Geniu$29",
-#             database = "Comp3161_lab_3")
-#         cursor = db.cursor()
-#         content = request.json
-#         # insert_queries()
-#         CustomerID = content["CustomerID"]
-#         Gender = content["Gender"]
-#         Age = content["Age"]
-#         Annual_Income = content["AnnualIncome"]
-#         Spending_Score = content["SpendingScore"]
-#         Profession = content["Profession"]
-#         Work_Experience = content["WorkExperience"]
-#         Family_Size = content["Family_Size"]
+
+@app.route('/add_customer', methods=['POST'])
+def add_customer():
+    try:
+        with db_connection() as db:
+            cursor = db.cursor()
+
+            content = request.json
+            customer_data = (
+                content.get("CustomerID"),
+                content.get("Gender"),
+                content.get("Age"),
+                content.get("AnnualIncome"),
+                content.get("SpendingScore"),
+                content.get("Profession"),
+                content.get("WorkExperience"),
+                content.get("FamilySize"),
+            )
+
+            cursor.execute(f"INSERT INTO customer (CustomerId, Gender, Age , Annual_Income, Spending_Score, Profession, Work_Experience, Family_Size ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);", customer_data)
+            db.commit()  
+
+            return make_response({"success": "Customer added"}, 201)
+
+    except Exception as e:
         
-#         cursor.executemany(f"INSERT INTO customer (CustomerId, Gender, Age , Annual_Income, Spending_Score, Profession, Work_Experience, Family_Size ) VALUES ({CustomerID}, '{Gender}', {Age} , {Annual_Income}, {Spending_Score}, '{Profession}', {Work_Experience}, {Family_Size}")
-#         db.commit()
-#         cursor.close()
-#         db.close()
-#         return make_response({"success" : "customer added"}, 201)
-#     except Exception as e:
-#         print(e)
-#         return make_response({'error': 'An error has occured'}, 400)
+        return make_response({'error': 'An error has occurred'}, 400)
+
 
 @app.route('/update_profession/<customer_id>', methods=['PUT'])
 def update_profession(customer_id):
@@ -129,10 +129,6 @@ def get_highest_income():
             customer_list = []
             
             for customer_id, annualIncome, profession in cursor:
-                # customer = {}                
-                # customer['AnnualIncome'] = annualIncome
-                # customer['customer_id'] = customer_id
-                # customer['Profession'] = profession
                 response_data = {
                     "CustomerID": customer_id,
                     "AnnualIncome": annualIncome,
@@ -145,65 +141,71 @@ def get_highest_income():
     except Exception as e:
          return make_response({'error': str(e)}, 400)
 
+    
+@app.route('/total_income_report', methods=['GET'])
+def get_total_income():
+    try:
+        with db_connection() as db:
+            cursor = db.cursor()
+            cursor.execute(f"select sum(Annual_Income) as TotalIncome, Profession from customer group by Profession ;")
+            customer_list = []
+            
+            for total_Income, profession in cursor:
+                response_data = {
+                    "Profession": profession,
+                    "TotalIncome": total_Income
+                    }
+                customer_list.append(response_data)
+            cursor.close()
+            db.close()
+            return make_response(customer_list, 200)
+    except Exception as e:
+         return make_response({'error': str(e)}, 400)
+     
+     
+@app.route('/average_spending_score/<profession>', methods=['GET'])
+def get_avg_spending_score(profession):
+    try:
+        with db_connection() as db:
+            cursor = db.cursor()
+            cursor.execute(f"SELECT Gender, AVG(Spending_Score) AS AverageSpendingScore FROM customer WHERE Profession = '{profession}' GROUP BY Gender;")
+            customer_list = []
+            
+            for gender, spendingScore in cursor:
+                response_data = {
+                    "Gender": gender,
+                    "AverageSpendingScore": spendingScore
+                    }
+                customer_list.append(response_data)
+            cursor.close()
+            db.close()
+            return make_response(customer_list, 200)
+    except Exception as e:
+         return make_response({'error': str(e)}, 400)
+     
+     
+     
+@app.route('/average_work_experience', methods=['GET'])
+def get_avg_experience():
+    try:
+        with db_connection() as db:
+            cursor = db.cursor()
 
-# @app.route('/highest_income_report', methods=['GET'])
-# def get_highest_income():
-#     try:
-#         with db_connection() as db:
-#             cursor = db.cursor()
+            cursor.execute(f"SELECT Profession, round(AVG(Work_Experience)) AS AverageExperience FROM customer WHERE Age < 35 AND Annual_Income > 50000 GROUP BY Profession;")
+            response_data = [
+                {
+                "Profession": profession, 
+                "AverageExperience": experience
+                 }
+                for profession, experience in cursor
+            ]
 
-#             # Find the customer with the highest income
-#             cursor.execute("SELECT CustomerID, Annual_Income, Profession FROM customer ORDER BY Annual_Income desc limit 3;")
-#             customer = cursor  # Fetch only the single highest-income row
+            return make_response(response_data, 200)
 
-#             # Check if a customer was found
-#             while customer is not None:
-#                 customer_id, annual_income, profession = customer
-#                 response_data = {
-#                     "CustomerID": customer_id,
-#                     "AnnualIncome": annual_income,
-#                     "Profession": profession
-#                 }
-#                 return make_response(response_data, 200)
-#             else:
-#                 # Handle the case where no customer exists (optional)
-#                 return make_response({'message': 'No customer found'}, 404)
+    except Exception as e:
+        print(e)
+        return make_response({'error': 'An error has occurred'}, 400)
 
-#     except Exception as e:
-#         return make_response({'error': str(e)}, 400)       
-
-# @app.route('/delete_customer/<customer_id>', methods=['DELETE'])
-# def delete_customer(customer_id):
-#     try:
-#         db = mysql.connector.connect(user='uwi_user', password='uwi876',
-#                                             host='127.0.0.1',
-#                                             database='uwi')
-#         cursor = db.cursor()
-#         cursor.execute(f"DELETE FROM customers WHERE customerID={customer_id}")
-#         db.commit()
-#         cursor.close()
-#         db.close()
-#         return make_response({"success" : "customer deleted"}, 200)
-#     except Exception as e:
-#         return make_response({'error': str(e)}, 400)
-
-
-# @app.route('/address_report', methods=['GET'])
-# def get_addresses():
-#     try:
-#         db = mysql.connector.connect(user='uwi_user', password='uwi876',
-#                                             host='127.0.0.1',
-#                                             database='uwi')
-#         cursor = db.cursor()
-#         cursor.execute(f"SELECT * FROM ALL_ADRESSESS")
-#         address_lst = []
-#         for add in cursor:
-#             address = {}
-#             address['Address'] = add[0]
-#             address_lst.append(address)
-#         return make_response(address_lst, 200)
-#     except Exception as e:
-#         return make_response({'error': str(e)}, 400)
     
 if __name__ == '__main__':
     app.run(port=5000)
